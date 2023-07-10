8fba7d02440923f8ef128360959670b0e640dc8c
varkenvarken <andersmichel8@gmail.com>
2023-07-10 17:08:43 +0200

    added user preferences to save address:port

--- src/blenderprometheus/__init__.py
+++ src/blenderprometheus/__init__.py
@@ -37,8 +37,12 @@ from sys import path
 from pathlib import Path
 path.append(str(Path(__file__).parent / "client_python"))
 
+import re
+valid_address = re.compile(r"(?P<address>.+):(?P<port>\d+)")
+
 import bpy 
 from bpy.app.handlers import persistent
+from bpy.props import StringProperty
 
 from prometheus_client import Gauge, Counter, REGISTRY
 from .server import start_server, stop_server
@@ -63,20 +67,63 @@ def render_post(scene=None):
     global frame_count
     frame_count.inc()
 
+class BlenderPrometheusPreferences(bpy.types.AddonPreferences):
+    bl_idname = __name__
+
+    address: StringProperty(
+        name="Address:port", description="address:port the metrics are exposed on", default="0.0.0.0:8000"
+    )
+
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.prop(self, "address")
+
+def start_serving():
+    # TODO sanity check on address:port
+    print(bpy.context.preferences.addons[__name__].preferences.address)
+    if match := valid_address.match(bpy.context.preferences.addons[__name__].preferences.address):
+        start_server(port=int(match.group("port")),addr=match.group("address"))
+    else:
+        start_server(8000)
+
+classes = (
+    BlenderPrometheusPreferences,)
 
 def register():
     """
     Create a Gauge and a Counter, then start a metrics server.
     """
+    for c in classes:
+        bpy.utils.register_class(c)
+
     global is_rendering
     is_rendering = Gauge("Blender_Render", "Rendering processes")
     global frame_count
     frame_count = Counter("Frame_Counter", "Number of frames rendered")
-    start_server(8000)
+
+    start_serving()
+
     bpy.app.handlers.render_cancel.append(render_cancel)
     bpy.app.handlers.render_complete.append(render_complete)
     bpy.app.handlers.render_init.append(render_init)
     bpy.app.handlers.render_post.append(render_post)
+
+    # Any Python object can act as the subscription's owner.
+    owner = object()
+    subscribe_to = bpy.context.preferences.addons[__name__].preferences.path_resolve("address", False)
+
+    def msgbus_callback(*args):
+        print(bpy.context.preferences.addons[__name__].preferences.address)
+        stop_server()
+        start_serving()
+
+
+    bpy.msgbus.subscribe_rna(
+        key=subscribe_to,
+        owner=owner,
+        args=tuple(),
+        notify=msgbus_callback,)
 
 def unregister():
     """
@@ -94,4 +141,7 @@ def unregister():
     REGISTRY.unregister(is_rendering)
     REGISTRY.unregister(frame_count)
     stop_server()
+
+    for c in classes:
+        bpy.utils.unregister_class(c)
 
